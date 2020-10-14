05150e3bb15734cc98b841dc48939d956a659888
Zzarate <zzarate@users.noreply.github.com>
2020-10-13 20:21:00 -0400

    Adicionado abertura de arquivo

--- Prog Paralela/T1/main.cpp
+++ Prog Paralela/T1/main.cpp
@@ -10,13 +10,17 @@
  */
 
 #include <iostream>
+#include "Arquivo.h"
 
 int main(int argc, char *argv[])
 {
     //Verifica se todos os parametros foram passados
-    if (argc < X)
+    if (argc < 3)
     {
-        std::cout << "Erro! Deve-se passar X argumentos:\n";
+        std::cout << "Erro! Deve-se passar 3 argumentos:\n";
         exit(1);
     }
+
+    Arquivo arq_entrada(argv[1], 'r'); //Cria um objeto e abre o arquivo passado como parametro
+    arq_entrada.Ler();                 //Le os caracteres do arquivo de entrada
 }
