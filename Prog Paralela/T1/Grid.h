231b0a49d0158d6a3099481481b793e6c0e23779
Zzarate <zzarate@users.noreply.github.com>
2020-10-13 22:33:52 -0400

    Adicionado obstaculos

--- Prog Paralela/T1/Grid.h
+++ Prog Paralela/T1/Grid.h
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <vector>
 
 typedef std::pair<unsigned int, unsigned int> celula; //Celula [i] [j];
 
@@ -8,11 +9,14 @@ class Grid
 private:
     unsigned int colunas;
     unsigned int linhas;
-    unsigned int qnt_obstaculos;
 
     celula origem;
     celula destino;
 
+    //Obstaculo
+    unsigned int qnt_obstaculos;
+    std::vector <celula> obsta_cel_init;
+ 
 public:
     Grid(/* args */);
     ~Grid();
