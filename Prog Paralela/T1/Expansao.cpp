8d764674b2ab23983f904af93dbd4146e37b3ef0
Huangnli <huang@DESKTOP-96V9H3L.localdomain>
2020-10-16 16:39:13 -0400

    atualizado

--- Prog Paralela/T1/Expansao.cpp
+++ Prog Paralela/T1/Expansao.cpp
@@ -1,6 +1,18 @@
 #include "Expansao.h"
 
 void expandir(celula origem) {
+
     fila.push (origem);
- 
+    while (fila.empty() != 0 && !achou) {
+        cel = remove(Fila) // Remove célula do início da fila
+        if (cel.i == destino.i AND cel.j == destino.j) // cel é o destino
+        achou = true
+    else {
+        for (cada célula viz vizinha de cel) // No máximo 4 vizinhos
+            if (Grid[viz.i][viz.j] == infinito) {
+                Grid[viz.i][viz.j] = Grid[cel.i][cel.j] + 1
+                insere(Fila, viz) // Insere célula viz no fim da fila
+            }
+        }
+    }
 }
