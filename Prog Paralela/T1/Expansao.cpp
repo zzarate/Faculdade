6d985c25df7facaa96e708cece45d5c9f391c9f4
Huangnli <50835276+Huangnli@users.noreply.github.com>
2020-10-18 20:20:35 -0400

    Prototipo do metodo vizinho

--- Prog Paralela/T1/Expansao.cpp
+++ Prog Paralela/T1/Expansao.cpp
@@ -1,4 +1,5 @@
 #include "Expansao.h"
+#include <limits.h>
 
 void Expansao::expandir(celula origem, celula destino) {
     celula cel;
@@ -7,17 +8,30 @@ void Expansao::expandir(celula origem, celula destino) {
     while (fila.empty() != 0 && !achou) {
         cel = fila.front();
         fila.pop(); // Remove célula do início da fila
-        if (cel.i == destino.i AND cel.j == destino.j) // cel é o destino
+        if (cel.i == destino.i && cel.j == destino.j) // cel é o destino
             achou = true;
-    else {
-        for (int k = 0; k < 4; k++) { // No máximo 4 vizinhos
+        else {
+            for (int k = 0; k < 4; k++) { // No máximo 4 vizinhos
             
-            if (Grid[viz.i][viz.j] == infinito) {
-                Grid[viz.i][viz.j] = Grid[cel.i][cel.j] + 1;
-                fila.push (viz); // Insere célula viz no fim da fila
+                if (Grid[viz.i][viz.j] == INT_MIN) {
+                    Grid[viz.i][viz.j] = Grid[cel.i][cel.j] + 1;
+                    fila.push (viz); // Insere célula viz no fim da fila
+                }
             }
         }
     }
+}
+
+int Expansao::vizinhoI(int k, celula cel) {
+    int i;
+    //...
+    return i;
+}
+int Expansao::vizinhoJ(int k, celula cel) {
+    int j;
+    //...
+    return j;
+}
 
 Expansao::Expansao(/* args */)
 {
--- Prog Paralela/T1/Expansao.h
+++ Prog Paralela/T1/Expansao.h
@@ -16,4 +16,6 @@ public:
     Expansao(/* args */);
     ~Expansao();
     void expandir(celula origem, celula destino);
+    int vizinhoI(int k, celula cel); //procurar o vizinho da celula e devover posicao i
+    int vizinhoJ(int k, celula cel); //procurar o vizinho da celula e devover posicao j
 };
