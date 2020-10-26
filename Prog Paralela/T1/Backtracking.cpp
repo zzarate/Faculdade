28eb163860a2a64e414ebe62c7678a4c70b231f8
Huangnli <huang@DESKTOP-96V9H3L.localdomain>
2020-10-26 18:27:48 -0400

    backtracking corrigido

--- Prog Paralela/T1/Backtracking.cpp
+++ Prog Paralela/T1/Backtracking.cpp
@@ -5,20 +5,23 @@ Backtracking::Backtracking(Grid grid, Expansao expand)
     if (expand.achou)
     {
         celula cel;
-        cel = grid.getDestino();
+        cel = grid.getDestino(); //coloca destino na cel
         pilha.push(grid.getDestino()); //coloca destino na pilha
 
         while (cel.first != grid.getOrigem().first || cel.second != grid.getOrigem().second) // Enquanto não chegar na origem
         {
-            for (int k = 0; k < 4; k++)
-            {                  // No máximo 4 vizinhos
+            for (int k = 0; k < 4; k++) // No máximo 4 vizinhos
+            {                  
                 long int i, j; //i e j do vizinho;
                 i = grid.vizinhoI(k, cel);
                 j = grid.vizinhoJ(k, cel);
-                grid.grid[i][j] == grid.grid[cel.first][cel.second] - 1;
-                celula viz(i, j);
-                cel = viz;
-                pilha.emplace(cel); // Insere célula viz na pilha
+                if(grid.grid[i][j] == grid.grid[cel.first][cel.second] - 1)
+                {
+                    celula viz(i, j); //cria uma celula vizinho 
+                    cel = viz;    
+                    pilha.emplace(cel); // Insere célula viz na pilha
+                }
+                
             }
         }
     }
