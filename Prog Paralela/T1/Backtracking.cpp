e2dd5dfad150316aa6449a398b6318ac49c02e7c
Zzarate <zzarate@users.noreply.github.com>
2020-10-27 17:05:11 -0400

    Pequenas modificações

--- Prog Paralela/T1/Backtracking.cpp
+++ Prog Paralela/T1/Backtracking.cpp
@@ -1,28 +1,33 @@
 #include "Backtracking.h"
 
+/**
+ * @brief Construct a new Backtracking:: Backtracking object
+ * 
+ * @param grid 
+ * @param expand 
+ */
 Backtracking::Backtracking(Grid &grid, Expansao &expand)
 {
     if (expand.achou)
     {
         celula cel;
-        cel = grid.getDestino(); //coloca destino na cel
+        cel = grid.getDestino();       //coloca destino na cel
         pilha.push(grid.getDestino()); //coloca destino na pilha
 
         while (cel.first != grid.getOrigem().first && cel.second != grid.getOrigem().second) // Enquanto não chegar na origem
         {
             for (int k = 0; k < 4; k++) // No máximo 4 vizinhos
-            {                  
+            {
                 long int i, j; //i e j do vizinho;
                 i = grid.vizinhoI(k, cel);
                 j = grid.vizinhoJ(k, cel);
-                
-                if(grid.grid[i][j] == grid.grid[cel.first][cel.second] - 1)
+
+                if (grid.grid[i][j] == grid.grid[cel.first][cel.second] - 1)
                 {
-                    celula viz(i, j); //cria uma celula vizinho 
-                    cel = viz;    
+                    celula viz(i, j); //cria uma celula vizinho
+                    cel = viz;
                     pilha.emplace(cel); // Insere célula viz na pilha
                 }
-                
             }
         }
     }
@@ -30,4 +35,16 @@ Backtracking::Backtracking(Grid &grid, Expansao &expand)
 
 Backtracking::~Backtracking()
 {
+}
+
+/**
+ * @brief Adiciona
+ * 
+ */
+void Backtracking::menorCaminho(){
+    menor_caminho.push_back(pilha.top().first);
+    menor_caminho+= ' ';
+    menor_caminho.push_back(pilha.top().second);
+    menor_caminho+= '\n';
+    pilha.pop();
 }
--- Prog Paralela/T1/Backtracking.h
+++ Prog Paralela/T1/Backtracking.h
@@ -11,6 +11,9 @@ class Backtracking
 {
 private:
     std::stack<celula> pilha; // Caminho da origem até o destino
+    std::string menor_caminho; //Menor caminho
+
+    void menorCaminho();
 
 public:
     Backtracking(Grid &grid, Expansao &expand);
