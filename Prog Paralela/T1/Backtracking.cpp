fc5d536623543ecbf1528d2b24f5a0888505a865
Huangnli <huang@DESKTOP-96V9H3L.localdomain>
2020-10-27 21:21:16 -0400

    atualizado

--- Prog Paralela/T1/Backtracking.cpp
+++ Prog Paralela/T1/Backtracking.cpp
@@ -23,7 +23,7 @@ Backtracking::Backtracking(Grid &grid, Expansao &expand)
         cel = grid.getDestino();       //coloca destino na cel
         pilha.push(grid.getDestino()); //coloca destino na pilha
 
-        while (cel.first != grid.getOrigem().first && cel.second != grid.getOrigem().second) // Enquanto não chegar na origem
+        while (cel.first != grid.getOrigem().first || cel.second != grid.getOrigem().second) // Enquanto não chegar na origem
         {
             for (int k = 0; k < 4; k++) // No máximo 4 vizinhos
             {
@@ -41,7 +41,6 @@ Backtracking::Backtracking(Grid &grid, Expansao &expand)
                 }
             }
         }
-        pilha.push(grid.getOrigem());
     }
 }
 
