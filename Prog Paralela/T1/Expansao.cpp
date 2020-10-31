88fcb419f769b7245b4fe408780e835de97fb061
Huangnli <50835276+Huangnli@users.noreply.github.com>
2020-10-31 16:42:01 -0400

    atualizado

--- Prog Paralela/T1/Expansao.cpp
+++ Prog Paralela/T1/Expansao.cpp
@@ -17,6 +17,7 @@
 Expansao::Expansao(Grid &grid)
 {
     fila.push(grid.getOrigem()); //coloca origem na fila
+    
     while (!(fila.empty()) && !(achou))
     {
         celula cel, celDest;
@@ -26,22 +27,43 @@ Expansao::Expansao(Grid &grid)
         if (cel.first == celDest.first && cel.second == celDest.second) // cel é o destino
             achou = true;
         else
-        {
-            for (int k = 0; k < 4; k++)
-            {                  // No máximo 4 vizinhos
-                long int i, j; //i e j do vizinho;
-                i = grid.vizinhoI(k, cel);
-                j = grid.vizinhoJ(k, cel);
-                if (i < grid.getLinhas() && j < grid.getColunas() && i >= 0 && j >= 0)
-                {
-                    if (grid.grid[i][j] == INT32_MAX)
+        {   
+            celula vizs[4];
+            #pragma omp parallel
+            {
+                #pragma omp for nowait
+                for (int k = 0; k < 4; k++)
+                {                  // No máximo 4 vizinhos
+                    long int i, j; //i e j do vizinho;
+                    i = grid.vizinhoI(k, cel);
+                    j = grid.vizinhoJ(k, cel);
+                    
+                    if (i < grid.getLinhas() && j < grid.getColunas() && i >= 0 && j >= 0)
                     {
-                        grid.grid[i][j] = grid.grid[cel.first][cel.second] + 1; //ver se a celula vizinho eh igual a celula atual
-                        celula viz(i, j);                                       //crio uma celula viziho
-                        fila.push(viz);                                         // Insere célula viz no fim da fila
+                        if (grid.grid[i][j] == INT32_MAX)
+                        {
+                            grid.grid[i][j] = grid.grid[cel.first][cel.second] + 1; 
+                            celula viz (i, j);                                       //crio uma celula viziho
+                            vizs[k] = viz;                                         // Insere célula viz no fim da fila
+                        }
+                        else
+                        {
+                            celula nViz (-1, -1);
+                            vizs[k] = nViz;
+                        }
+                    }
+                    else
+                    {
+                        celula nViz (-1, -1);
+                        vizs[k] = nViz;
                     }
                 }
             }
+            for(int k = 0; k < 4; k++){
+                if(vizs[k].first != -1 || vizs[k].second != -1)
+                    fila.push(vizs[k]);
+            }
+
         }
     }
 }
