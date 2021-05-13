96097a9fe2b7b17c7e2f022b5096786d3b8c3ffc
Huangnli <50835276+Huangnli@users.noreply.github.com>
2021-05-12 22:03:17 -0400

    add sigint

--- Sistemas Operacionais/Trabalho 1/src/sh.cpp
+++ Sistemas Operacionais/Trabalho 1/src/sh.cpp
@@ -4,16 +4,25 @@
 
 #include "include/sh.h"
 
+static void end_handler(int signal) {
+    // printf("MyShell$ ");
+}
+
 int main (int argc, char *argv[])
 {
     char cmd[100];
+
+    if ((signal(SIGINT, end_handler) == SIG_ERR)) {
+        printf("Error while setting a signal handler\n");
+        exit(EXIT_FAILURE);
+    }   
 
     while (1)
     {
         printf("\nMyShell$ ");
         fgets(cmd, 100, stdin);
 
-        if (strncmp(cmd, "exit", 4) == 0 || strncmp(cmd, "$", 1) == 0) { //FIXME
+        if (strncmp(cmd, "exit", 4) == 0 || strncmp(cmd, "^D", 2) == 0) { //FIXME
             exit(0);
         }
     }
